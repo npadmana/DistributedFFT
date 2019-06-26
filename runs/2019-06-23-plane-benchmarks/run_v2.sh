734b9b6e6b5ea8b6a77383e07c1bf3456e453958
Nikhil Padmanabhan <nikhil.padmanabhan@gmail.com>
2019-06-26 09:39:08 -0500

    Timings

---
+++ runs/2019-06-23-plane-benchmarks/run_v2.sh
@@ -0,0 +1,11 @@
+#PBS -l select=16:ncpus=44
+#PBS -l walltime=01:00:00
+#PBS -j oe
+#PBS -o plane-communication_v2
+
+# Run the default version for the plane communication code
+cd $PBS_O_WORKDIR
+
+for NG in 128 512 1024 1536 2048 2560; do
+    ../../target/example/plane_v2 -nl 16 --Ng=$NG
+done
