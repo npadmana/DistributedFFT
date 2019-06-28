d71f66baebdbb9516f8151274ab9dba04acedcd9
Nikhil Padmanabhan <nikhil.padmanabhan@gmail.com>
2019-06-28 17:16:30 -0500

    Create benchmark file

--- runs/2019-06-28-distfft/run.sh
+++ runs/2019-06-28-distfft/run.sh
@@ -1,7 +1,7 @@
 #PBS -l select=16:ncpus=44
 #PBS -l walltime=01:00:00
 #PBS -j oe
-#PBS -o fftw-mpi-benchmark.out
+#PBS -o distributed-benchmark.out
 
 # Run the default version for the plane communication code
 cd $PBS_O_WORKDIR
